f8ee897126e77c64c495e947b7a126b625020aac
Yen <pnf4665jds@gmail.com>
2020-04-02 20:37:04 +0800

    嘗試製作怪物追蹤功能

---
+++ 3DGame/Assets/Scripts/Monster/DetectPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+    public bool ShowInScene;
+    public float DetectLength = 5;
+    public float StopLength = 10;    // 偵測到玩家後，玩家與怪物距離多遠時停止偵測
+    public GameObject PlayerObj { get; set; }
+
+    private void Update()
+    {
+        Detect();
+    }
+
+    /// <summary>
+    /// 偵測玩家
+    /// </summary>
+    private void Detect()
+    {
+        Collider[] playerCollider = Physics.OverlapSphere(transform.position, DetectLength, LayerMask.GetMask("Player"));
+        if (playerCollider.Length > 0)
+        {
+            PlayerObj = playerCollider[0].gameObject;
+        }
+        else if(PlayerObj != null && Vector3.Distance(transform.position, PlayerObj.transform.position) > StopLength)
+        {
+            PlayerObj = null;
+        }
+    }
+
+    /// <summary>
+    /// 在Scene中繪製對應的範圍
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        if (!ShowInScene)
+            return;
+
+        Gizmos.color = new Color(0, 1, 0, 0.4f);
+        Vector3 target = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0) * new Vector3(0, 0, DetectLength);
+        Gizmos.DrawSphere(transform.position, DetectLength);
+    }
+}
---
+++ 3DGame/Assets/Scripts/Monster/FollowAndAttack.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAndAttack : MonoBehaviour
+{
+    public DetectPlayer DetectPlayer;
+    public float MoveSpeed;
+    public float RotateSpeed;
+    public float AttackDistance;
+
+    private GameObject target;
+    private bool ReadyToAttack = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DetectPlayer.PlayerObj != null)
+        {
+            target = DetectPlayer.PlayerObj;
+            if (Vector3.Distance(transform.position, target.transform.position) <= AttackDistance)
+            {
+                AttackAnim();
+            }
+            else
+            {
+                Follow();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 跟隨玩家
+    /// </summary>
+    private void Follow()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, MoveSpeed * Time.deltaTime);
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, target.transform.position - transform.position, RotateSpeed * Time.deltaTime, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+    /// <summary>
+    /// 播放攻擊動畫
+    /// </summary>
+    private void AttackAnim()
+    {
+        GetComponent<Animation>().Play("Anim_Attack");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit");
+    }
+}
