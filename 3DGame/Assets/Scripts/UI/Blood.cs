fbee3c158c3a2b97bf86f6c9429acaf47f464bf2
Bruce <bruce6531@gmail.com>
2020-04-04 12:17:10 +0800

    增加UI

---
+++ 3DGame/Assets/Scripts/UI/Blood.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blood : MonoBehaviour
+{
+    public float MaxBlood;
+    public float curBlood;
+    /// <summary>
+    /// 如果血條X軸座標移動至-92.6判定死亡
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxBlood = 100;
+        curBlood = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localPosition = new Vector3( (-92.6f + 92.6f * (curBlood / MaxBlood)) , 0.0f  ,0.0f);
+    }
+}
---
+++ 3DGame/Assets/Scripts/UI/MP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MP : MonoBehaviour
+{
+    public float MaxMp;
+    public float curMp;
+    private Image MpBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MpBar = this.GetComponent<Image>();
+        MaxMp = 100.0f;
+        curMp = 100.0f;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        MpBar.fillAmount = curMp / MaxMp;
+    }
+}
