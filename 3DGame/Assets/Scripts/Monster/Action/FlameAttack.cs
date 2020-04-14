0fdcd6169b4496b16d1d55f438cb8c7080d14bcd
Yen <pnf4665jds@gmail.com>
2020-04-14 12:56:16 +0800

    update flame effect

--- 3DGame/Assets/Scripts/Monster/Action/FlameAttack.cs
+++ 3DGame/Assets/Scripts/Monster/Action/FlameAttack.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class FlameAttack : ActionBase
 {
+    // 火焰特效物件
+    public GameObject FlameObject;
+    // 龍的頭部
+    public GameObject Head;
+    // 微調噴火位置用
+    public Vector3 Offset;
+    // 特效出現的延遲
+    public float DelayBeforeEffect;
+    // 特效持續時間
+    public float KeepTime;
+
     private bool colDetect = false;
+    private GameObject flameObject;
 
     public override void Init()
     {
         colDetect = true;
         animator.SetTrigger("FlameAttack");
+        StartCoroutine(StartEffect());
     }
 
     public override void Process()
@@ -20,6 +33,21 @@ public class FlameAttack : ActionBase
     public override void Exit()
     {
         colDetect = false;
+    }
+
+    /// <summary>
+    /// 經過Delay後產生火焰特效物件
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator StartEffect()
+    {
+        yield return new WaitForSeconds(DelayBeforeEffect);
+        flameObject = Instantiate(FlameObject, Head.transform);
+        yield return new WaitForSeconds(KeepTime);
+        // 先停止產生粒子，並等待剩餘粒子消失後再破壞物體
+        flameObject.GetComponent<ParticleSystem>().Stop();
+        yield return new WaitForSeconds(5);
+        Destroy(flameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
