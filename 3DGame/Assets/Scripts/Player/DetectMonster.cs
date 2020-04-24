713c9c77e9c42d76ec32198b6952081d5ec081b2
Reese <mornuing@gmail.com>
2020-04-25 00:05:31 +0800

    腳色部分功能

---
+++ 3DGame/Assets/Scripts/Player/DetectMonster.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectMonster : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Monster")
+        {
+            Debug.Log(collision.gameObject.tag);
+        }
+    }
+}
--- 3DGame/Assets/Scripts/Player/Player.cs
+++ 3DGame/Assets/Scripts/Player/Player.cs
@@ -9,11 +9,28 @@ public class Player : MonoBehaviour
     public bool isHit;
     public bool isMove;
     public bool isDie;
+    private bool isLive;
+    private bool isSetMousePos;
     private float Speed;
+    private float MaxHP;
+    private float CurrentHP;
+    private float Mana;
+    private GameObject PlayerBloodUi;
+    private GameObject MPUI;
+    private Vector3 MouseStartPos;
+
+    private void Awake()
+    {
+        PlayerBloodUi = GameObject.FindGameObjectWithTag("PlayerBloodUI");
+        MPUI = GameObject.FindGameObjectWithTag("MPUI");
+        Playerani = GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        Playerani = GetComponent<Animator>();
+        CurrentHP = MaxHP;
+        isLive = true;
+        isSetMousePos = false;
         isAttack = false;
         isHit = false;
         isMove = false;
@@ -24,43 +41,84 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isMove)
+        if (isLive)
         {
-            if (Speed >= 1)
+            if (isMove)
             {
-                Speed = 1;
+                if (Speed >= 1)
+                {
+                    Speed = 1;
+                }
+                else Speed += Time.deltaTime;
             }
-            else Speed += Time.deltaTime;
+            else Speed = 0;
+            if (Input.GetMouseButton(0))
+            {
+                isAttack = true;
+                Playerani.SetBool("isAttack", isAttack);
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                ResetAnimation();
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                isMove = true;
+                transform.Translate(Vector3.forward * Speed * 0.1f);
+                Playerani.SetBool("isMove", isMove);
+            }
+            else if (Input.GetKeyUp(KeyCode.W))
+            {
+                ResetAnimation();
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(transform.up * 0.5f);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(transform.up * -0.5f);
+            }
+            if (Input.GetMouseButton(1))
+            {
+                if (!isSetMousePos)
+                {
+                    MouseStartPos = Input.mousePosition;
+                    isSetMousePos = true;
+                }
+                Vector3 dis = Input.mousePosition - MouseStartPos;
+                if(dis.x >20 || dis.x < -20)
+                {
+                    this.transform.GetChild(2).transform.Rotate(new Vector3(0,dis.x*0.01f,0));
+                }
+                else if(dis.y >20 || dis.y < -20)
+                {
+                    this.transform.GetChild(2).transform.Rotate(new Vector3(dis.y*0.01f,0,0));
+                }
+            }
+            else if (Input.GetMouseButtonUp(1))
+            {
+                isSetMousePos = false;
+            }
         }
-        else Speed = 0;
-        if (Input.GetMouseButton(0))
+    }
+    public void GetDamage(float damage) {
+        CurrentHP -= damage;
+        PlayerBloodUi.transform.GetChild(0).transform.GetChild(0).GetComponent<PlayerBlood>().GetHurt(damage);
+        if(CurrentHP <=0)
         {
-            isAttack = true;
-            Playerani.SetBool("isAttack",isAttack);
+            isLive = false;
+            isDie = true;
+            Playerani.SetBool("isDie", isDie);
         }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            isAttack = false;
-            Playerani.SetBool("isAttack",isAttack);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            isMove = true;
-            transform.Translate(Vector3.forward * Speed*0.1f);
-            Playerani.SetBool("isMove",isMove);
-        }
-        else if (Input.GetKeyUp(KeyCode.W))
-        {
-            isMove = false;
-            Playerani.SetBool("isMove",isMove);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(transform.up * 0.5f);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(transform.up * -0.5f);
-        }
+    }
+    public void ResetAnimation()
+    {
+        isAttack = false;
+        isHit = false;
+        isMove = false;
+        Playerani.SetBool("isAttack",isAttack);
+        Playerani.SetBool("isHit",isHit);
+        Playerani.SetBool("isMove", isMove);
     }
 }
