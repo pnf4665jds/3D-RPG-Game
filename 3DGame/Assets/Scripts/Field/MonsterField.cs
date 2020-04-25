1354295c115d5c61a6e6eb308c087c03d9b26dc3
Bruce <bruce6531@gmail.com>
2020-04-25 21:32:10 +0800

    add monster field

---
+++ 3DGame/Assets/Scripts/Field/MonsterField.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterField : MonoBehaviour
+{
+    public string Name;
+    
+    public float radius {
+        get {
+            return this.GetComponent<SphereCollider>().radius;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+    public string GetFieldName() {
+        return Name;
+    }
+
+}
