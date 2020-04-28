2a47fba3191c61d71dde16a637558955610c4826
Bruce <bruce6531@gmail.com>
2020-04-29 01:28:11 +0800

    moneySystem

---
+++ 3DGame/Assets/Scripts/Monster/RockBallDamage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBallDamage : MonoBehaviour
+{
+    public Transform Rock;
+    public void createRock() {
+        Vector3 newPos = new Vector3(this.transform.position.x, this.transform.position.y + 3, this.transform.position.z);
+        var rock = Instantiate(Rock, newPos, Quaternion.identity);
+        rock.transform.rotation = this.transform.rotation;
+    }
+}
---
+++ 3DGame/Assets/Scripts/Monster/RockBallRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBallRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Golem;
+    public void rotate() {
+        this.transform.GetComponentInParent<Transform>().rotation = Golem.transform.rotation;
+    }
+}
