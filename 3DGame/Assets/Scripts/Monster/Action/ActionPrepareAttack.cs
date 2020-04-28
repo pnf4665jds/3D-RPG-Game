06579f232c01cf9f57cfbfbf26ba571e8e5e1d32
Bruce <bruce6531@gmail.com>
2020-04-28 20:33:39 +0800

    new golem ai

---
+++ 3DGame/Assets/Scripts/Monster/Action/ActionPrepareAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionPrepareAttack : ActionBase
+{
+    // Start is called before the first frame update
+
+    public override void Init()
+    {
+        animator.SetBool("PrepareAttack", true);
+       
+    }
+
+    public override void Process()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+    }
+}
