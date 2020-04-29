b3f94b0eae0545477665324b0403e8eb0e12abb4
Bruce <bruce6531@gmail.com>
2020-04-29 15:02:03 +0800

    throw The Rock

---
+++ 3DGame/Assets/Scripts/Monster/RockMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockMove : MonoBehaviour
+{
+    private GameObject player;
+    public float speed = 10 ;
+    private float distanceToPlayer;
+    private bool move = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        distanceToPlayer = Vector3.Distance(this.transform.position, player.transform.position);
+        StartCoroutine(shoot());
+
+    }
+
+    public IEnumerator shoot() {
+        Vector3 playerPos = player.transform.position;
+        
+        while (move) {
+            this.transform.LookAt(playerPos);
+            float angle = Mathf.Min(1, Vector3.Distance(this.transform.position, playerPos) / distanceToPlayer) * 45;
+            this.transform.rotation = this.transform.rotation * Quaternion.Euler(Mathf.Clamp(-angle, -42, 42), 0, 0);
+            float currentDist = Vector3.Distance(this.transform.position, player.transform.position);
+            if (currentDist < 0.5f || this.transform.position.y  < 2)
+            {
+                move = false;
+                Destroy(this.gameObject);
+            }
+            this.transform.Translate(Vector3.forward * Mathf.Min(speed * Time.deltaTime, currentDist));
+            yield return null;
+        }
+        
+    }
+}
