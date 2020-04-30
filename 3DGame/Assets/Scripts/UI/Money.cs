d8b3b74e9b6ad7da7cca7b711f2b18ca352888c4
Bruce <bruce6531@gmail.com>
2020-05-01 00:32:42 +0800

    add story script and moneyUI

---
+++ 3DGame/Assets/Scripts/UI/Money.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+    private int totalMoney = 100;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //totalMoney = player.GetComponent<Player>().GetMoney();
+        this.GetComponent<Text>().text = totalMoney.ToString();
+    }
+}
