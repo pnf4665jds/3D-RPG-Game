6909c783e3d23a49a2d92f9f55adc946e31def3a
Bruce <bruce6531@gmail.com>
2020-05-01 11:35:25 +0800

    修改成dropItemSystem

---
+++ 3DGame/Assets/Scripts/DropItems/DropItemSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItemSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject MoneyPref;
+    public GameObject HealthPref;
+    public GameObject ManaPref;
+
+    public void createMoney(Vector3 pos , int number) {
+
+        for (int i = 0; i < number; i++) {
+            Instantiate(MoneyPref, pos, Quaternion.identity);
+        }
+
+    }
+    public void createHealthItem(Vector3 pos, int number)
+    {
+
+        for (int i = 0; i < number; i++)
+        {
+            Instantiate(HealthPref, pos, Quaternion.identity);
+        }
+
+    }
+    public void createManaItem(Vector3 pos, int number)
+    {
+
+        for (int i = 0; i < number; i++)
+        {
+            Instantiate(ManaPref, pos, Quaternion.identity);
+        }
+
+    }
+}
