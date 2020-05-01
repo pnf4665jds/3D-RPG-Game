dbc49430e3e3b9d014bd14b4f8e027afc9bf96e4
Bruce <bruce6531@gmail.com>
2020-05-02 00:08:36 +0800

    修改golem大小 及 石頭丟的速度

--- 3DGame/Assets/Scripts/Monster/RockMove.cs
+++ 3DGame/Assets/Scripts/Monster/RockMove.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RockMove : MonoBehaviour
 {
     private GameObject player;
-    public float speed = 10 ;
+    public float speed  ;
     private float distanceToPlayer;
     private bool move = true;
 
