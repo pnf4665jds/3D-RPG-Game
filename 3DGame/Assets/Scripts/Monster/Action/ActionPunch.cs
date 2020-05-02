146dc696fe675a9cea2454b52ffd6cde32e05fab
Bruce <bruce6531@gmail.com>
2020-05-02 16:28:33 +0800

    add punch damage

---
+++ 3DGame/Assets/Scripts/Monster/Action/ActionPunch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionPunch : ActionBase
+{
+    public float Damage;    // 傷害
+    public GameObject Punch;
+    private bool alreadyHit = true;  // 是否已經攻擊到，防止過程中多次判定
+
+    // Start is called before the first frame update
+    public override void Init()
+    {
+        animator.SetTrigger("Attacking");
+        setPunchDamage(Damage);
+    }
+
+    public override void Process()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public void setPunchDamage(float hurtValue) {
+        Punch.GetComponent<PunchDamager>().SetValue(gameObject, hurtValue);
+    }
+}
---
+++ 3DGame/Assets/Scripts/Monster/PunchDamager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchDamager : MonoBehaviour
+{
+    private GameObject userObject;
+    private float finalDamage;
+    // Start is called before the first frame update
+    public void SetValue(GameObject user, float damage)
+    {
+        userObject = user;
+        finalDamage = damage;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Player>().GetHurt(finalDamage);
+            //Debug.Log(other.gameObject.GetComponent<Player>().GetCurHP());
+        }
+    }
+}
