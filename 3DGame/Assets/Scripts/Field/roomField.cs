bb1856dc95a5b03e04b95109d765244e407ad2ef
Bruce <bruce6531@gmail.com>
2020-05-08 00:56:02 +0800

    trap system

---
+++ 3DGame/Assets/Scripts/Field/roomField.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomField : MonoBehaviour
+{
+    public List<trapGroup> tgs = new List<trapGroup>();
+    private bool isPlayerExisted = false;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        /*foreach (trapGroup tg in tgs)
+        {
+            StartCoroutine(tg.switchON());
+        }*/
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "Player") {
+            Debug.Log("Enter");
+            
+                foreach (trapGroup tg in tgs)
+                {
+                    StartCoroutine(tg.switchON());
+                }
+            
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isPlayerExisted = true;
+            foreach (trapGroup tg in tgs)
+            {
+                tg.switchOFF();
+            }
+        }
+    }
+
+}
---
+++ 3DGame/Assets/Scripts/trap/trapGroup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trapGroup : MonoBehaviour
+{
+    public List<GameObject> traps = new List<GameObject>();
+    public float DeltaTime;
+
+    public float getDeltaTime() {
+        return DeltaTime;
+    }
+    public IEnumerator switchON() {
+
+        yield return new WaitForSeconds(DeltaTime);
+        foreach (GameObject trap in traps) {
+            trap.GetComponent<Animation>().Play();
+            
+        }
+    }
+    public void switchOFF() {
+        foreach (GameObject trap in traps)
+        {
+            trap.GetComponent<Animation>().Stop() ;
+
+        }
+    }
+    
+}
