f2bdc99ae774547f43aadd110fae77f7168dcb8f
Bruce <bruce6531@gmail.com>
2020-05-15 21:29:45 +0800

    detect the player to move robot

--- 3DGame/Assets/Scripts/Shop/shopRobot.cs
+++ 3DGame/Assets/Scripts/Shop/shopRobot.cs
@@ -11,16 +11,39 @@ public class shopRobot : MonoBehaviour
        
         anim = this.GetComponent<Animator>();
         anim.SetBool("Open_Anim", true);
+
+    }
+    private void FixedUpdate()
+    {
+        LookAtPlayer();
     }
 
+    private void LookAtPlayer() {
+        Collider[] enterZone = Physics.OverlapBox(transform.position , transform.localScale/2 , transform.rotation, LayerMask.GetMask("Player"));
+
+        if (enterZone.Length > 0) {
+            var playerEntity = GameObject.FindGameObjectWithTag("Player");
+            transform.LookAt(playerEntity.transform);
+            if (playerEntity.GetComponent<Player>()) {
+                anim.SetBool("Walk_Anim", true);
+            }
+            else {
+                anim.SetBool("Walk_Anim", false);
+            }
+            
+
+            
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            
+            anim.SetBool("Walk_Anim", false);
             StartCoroutine(animFlow());
         }
     }
+
     private IEnumerator animFlow() {
         anim.SetBool("Open_Anim", true);
         GameSystem.instance.changeGameState();
@@ -31,5 +54,10 @@ public class shopRobot : MonoBehaviour
         yield return new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName("anim_close"));
         GameSystem.instance.changeGameState();
         yield return null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
     }
 }
