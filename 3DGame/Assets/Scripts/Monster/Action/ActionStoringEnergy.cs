0d6bad5018ef8ff23f30cc9cc0f1c7805b941d34
Yen <pnf4665jds@gmail.com>
2020-05-18 00:02:39 +0800

    Make boss to aim target when storing energy

--- 3DGame/Assets/Scripts/Monster/Action/ActionStoringEnergy.cs
+++ 3DGame/Assets/Scripts/Monster/Action/ActionStoringEnergy.cs
@@ -14,11 +14,13 @@ public class ActionStoringEnergy : ActionBase
 
     private GameObject storingObject;
     private GameObject storingBall;
+    private GameObject target;
 
     public override void Init()
     {
+        target = GameObject.FindWithTag("Player");
         storingObject = Instantiate(StoringObject, transform.position + Vector3.up, Quaternion.identity);
-        storingBall = Instantiate(StoringBall, ShootPosition.transform.position, Quaternion.identity);
+        storingBall = Instantiate(StoringBall, ShootPosition.transform.position, Quaternion.identity, transform);
         foreach (BoltPoints points in BoltPointsList)
         {
             LightningBoltScript lightning = Instantiate(LightningEffect).GetComponent<LightningBoltScript>();
@@ -30,12 +32,20 @@ public class ActionStoringEnergy : ActionBase
     public override void Process()
     {
         storingObject.transform.localScale -= new Vector3(15f, 0, 15f) / StoringTime * Time.deltaTime;
+        AimTarget();
     }
 
     public override void Exit()
     {
         Destroy(storingObject);
         Destroy(storingBall);
+    }
+
+    private void AimTarget()
+    {
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, target.transform.position - transform.position, monsterInfo.RotateSpeed * Time.deltaTime, 0.0f);
+        newDirection.y = 0;
+        transform.rotation = Quaternion.LookRotation(newDirection);
     }
 }
 
