f7b6f0afcd8cada31b6d3c156ef12b7402037139
Bruce <bruce6531@gmail.com>
2020-06-03 21:25:41 +0800

    combine the story and TimeLine System

--- 3DGame/Assets/Scripts/System/StorySystem.cs
+++ 3DGame/Assets/Scripts/System/StorySystem.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 public class StorySystem : Singleton<StorySystem>
 {
     private bool StoryShow = false;
+    private GameObject player;
+    [SerializeField]
+    private PlayableDirector DirectorEnterBossZone;
+    [SerializeField]
+    private PlayableDirector DirectorExistBossZone;
+    [SerializeField]
+
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         StartCoroutine(StoryFlowControl());
     }
 
@@ -16,23 +25,61 @@ public class StorySystem : Singleton<StorySystem>
     public IEnumerator StoryFlowControl() {
         for (int i = 1; i <= SceneManager.sceneCount; i++) {
             yield return new WaitUntil(() => GameSceneManager.instance.IsLoadingFinish);
-            //StoryShow = true; //告知玩家，讓玩家call GameSystem to change to the Story mode
+
+            yield return new WaitUntil(() => findObjectInScene());
+
             GameSystem.instance.changeModeStory();
-            UISystem.instance.getStoryPanel().GetComponent<storyUI>().setloadingFinshFile(i);
-            yield return new WaitForSecondsRealtime(3);
-            //StoryShow = false;
+            UISystem.instance.getStoryPanel().GetComponent<storyUI>().setloadingFinshFile(i); //設定劇情
+
+            yield return new WaitForSecondsRealtime(2);
+            yield return new WaitUntil(() => Input.GetKey(KeyCode.Z)); //關閉劇情
+
             GameSystem.instance.changeModeFollowPlayer();
+
+            yield return new WaitUntil(() => player.GetComponent<Player>().GetisInBoss()); //進入boss場景
+            yield return new WaitForSeconds(1);
+
+            GameSystem.instance.changeModeAnimation();
+            TimeLinePlay(DirectorEnterBossZone);
+
+            yield return new WaitUntil(() => isTimeLineCompleted(DirectorEnterBossZone)); //結束進入boss場景的動畫
+
+            GameSystem.instance.changeModeFollowPlayer();
+
             yield return new WaitUntil(() => MonsterSystem.instance.IsBossDead);
-            // StoryShow = true; //告知玩家，讓玩家call GameSystem to change to the Story mode
+
+            GameSystem.instance.changeModeAnimation();
+            TimeLinePlay(DirectorExistBossZone);
+
+            yield return new WaitUntil(() => isTimeLineCompleted(DirectorExistBossZone)); //結束boss死亡後的動畫
+
             GameSystem.instance.changeModeStory();
             UISystem.instance.getStoryPanel().GetComponent<storyUI>().setbossDeadFile(i);
-            yield return new WaitForSecondsRealtime(3);
-            //StoryShow = false;
+
+            yield return new WaitForSecondsRealtime(2);
+
+            yield return new WaitUntil(() => Input.GetKey(KeyCode.Z)); //關閉劇情
+
             GameSystem.instance.changeModeFollowPlayer();
         }
     }
-   
-    public bool isTheStoryShow() {
-        return StoryShow;
+
+    public void TimeLinePlay(PlayableDirector temp)
+    {
+        temp.Play();
+
+    }
+    public bool isTimeLineCompleted(PlayableDirector temp)
+    {
+        return temp.state != PlayState.Playing;
+    }
+    public bool findObjectInScene()
+    {
+
+        DirectorEnterBossZone = GameObject.FindGameObjectWithTag("EnterBossFieldAnim").GetComponent<PlayableDirector>();
+        DirectorExistBossZone = GameObject.FindGameObjectWithTag("BossDeadAnim").GetComponent<PlayableDirector>();
+
+        return true;
+
     }
 }
